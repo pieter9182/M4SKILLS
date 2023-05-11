55127b33317db338722db15e47f21e60b68a3aa3
damian <34666@ma-web.nl>
2023-05-11 10:45:46 +0200

    php files

---
+++ structuur/private/afhandeling.php
@@ -0,0 +1,7 @@
+<?php
+var_dump($_GET);
+    $hoi = "hoi";
+    echo "<h1>" .$hoi."</h1>";
+    echo $_GET['email'];  /* de [] kiest iets speciect uit de array die er voor staat ($_get)  */
+    echo $_GET['voornaam'];
+?>
